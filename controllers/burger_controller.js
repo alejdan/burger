ab2ae05276f90f22ff2f479030cc6c1f344295d4
alejdan <alejdan@gmail.com>
2019-06-14 15:46:27 -0500

    Setting up the directories

---
+++ controllers/burger_controller.js
@@ -0,0 +1,5 @@
+var app = require("express");
+
+var burger = require("burger.js");
+
+// Create the`router` for the app, and export the`router` at the end of your file.
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("cubana", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("callejera", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("big mac", false);
