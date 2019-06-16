9fa1d1c324ed2a63023e47b7effb9cc7e580a743
alejdan <alejdan@gmail.com>
2019-06-15 21:01:56 -0500

    Added extra needed data

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "Tanocondor_7",
     database: "burgers_db",
     pool:10
 });
